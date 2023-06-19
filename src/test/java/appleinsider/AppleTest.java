273b4794b50cbc72fd6443bb9f027cf962e03837
vsafontev <vladislavsafontev@gmail.com>
2023-06-19 14:16:20 +0300

    3 commit

--- src/test/java/appleinsider/AppleTest.java
+++ src/test/java/appleinsider/AppleTest.java
@@ -1,9 +1,11 @@
 package appleinsider;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 
-public class AppleTest  {
+import core.BaseTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppleTest extends BaseTest {
 
     private final static String BASE_URL = "https://appleinsider.ru/";
     private final static String SEARCH_STRING = "Чем iPhone 13 отличается от iPhone 12";
@@ -11,9 +13,10 @@ public class AppleTest  {
 
     @Test
     public void checkHref(){
-        Assertions.assertTrue(new MainPage(BASE_URL)
+        Assert.assertTrue(new MainPage(BASE_URL)
                 .search(SEARCH_STRING)
                 .getHrefFromFirstArticle()
                 .contains(EXPECTED_WORD));
     }
 }
+
